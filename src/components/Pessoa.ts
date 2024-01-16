7338aa82ba6ad726c4dbd7ed62fc02b1065f5fea
Luiz Iury <luiziury_ios@hotmail.com>
2024-01-16 15:03:23 -0300

    Usando Props e Hooks

---
+++ src/components/Pessoa.ts
@@ -0,0 +1,9 @@
+interface PessoaProps{
+    nome: string
+    idade: string
+    nacionalidade: string
+}
+
+// nome, idade, nacionalidade
+let Pessoa: PessoaProps
+
--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -1,11 +1,33 @@
+import { useEffect, useState } from 'react'
 import './Task.css'
 
 function Task() {
+  const [completado, setCompletado] = useState(false) //boolean
+  const [tarefa, setTarefa] = useState('') //string
+
+  // Função a ser disparada / Um gatilho (algo a ser analizado)
+  useEffect(() => {
+      if(completado == true){
+        setTarefa("Tarefa Concluída")
+      }
+    }, [completado])
+
+  /*
+   function () {} == () => {}
+  */
+
+   /*
+   function concluiTarefa(){         onClick={() => {setCompletado(true)}}             
+    setCompletado(true)         == 
+   }
+   */
+
+
   return (
     <div className="container">
         <h1>Tarefa</h1>
-        <h3>Status da Tarefa</h3>
-        <button>Conclua a Tarefa</button>
+        <h3>Status da Tarefa: { tarefa }</h3>
+        <button onClick={() => {setCompletado(true)}}>Conclua a Tarefa</button>
     </div>
   )
 }
